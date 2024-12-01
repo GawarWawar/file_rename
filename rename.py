8aebeb9a9695c555313545a2e51271c1fdefd260
GawarWawar <gawarwawar@gmail.com>
2024-12-02 01:52:09 +0200

    feat(structure): Make a separate function for get_ids that uploads csv to DF

--- rename.py
+++ rename.py
@@ -18,26 +18,37 @@ def get_config() -> dict[str]:
     with open("config.json") as file_config:
         return json.load(file_config)
     
+    
+def get_ids(name_of_csv_with_ids: str) -> pd.DataFrame:
+    """Make a list of ids from presented path to csv file.
+    Column at 0 position - should be a column with all off ids. Only this column matter.
+        CSV can contain other columns and it wont affect the process.
+
+    Args:
+        name_of_csv_with_ids (str): CSV file with ids of products
+
+    Returns:
+        pd.DataFrame: DF with ids
+    """
+    return pd.read_csv(name_of_csv_with_ids)
+    
 def rename_directory(
     path_to_directory: str, 
-    name_of_file_with_ids: str
+    ids_df: pd.DataFrame,
 ) -> None:
-    """Find all files in path_to_directory and tries to copy them as images in png format
+    """Find all files in path_to_directory and tries to copy them as images in png format with a new name.
+    Name contain id: from ids DF; part_code: file should have a name which is composed of that part code; and file extension - png.
+    So images will have a name as: id.part_code.png
 
     Args:
         path_to_directory (str): Absolute path to the directory
         name_of_file_with_ids (str): Name of file with ids
     """
-    # Find all files in config["path_to_folder_with_pictures"]
+    # Find all files in path_to_directory
     files = [f for f in pathlib.Path(path_to_directory).iterdir() if f.is_file()]
-
-    # Make a list of ids from config["name_of_file_with_ids"]
-    ids = pd.read_csv(name_of_file_with_ids)
-    # ASIN - column with all off ids. Only this column matter
-        # CSV can contain other columns and it wont affect the process 
         
     # For every id we create a copy of each part
-    for id in ids.iloc[0]:
+    for id in ids_df.iloc[0]:
         for file in files:
             # Each image should have a part code as a name 
             part_code = file.name.removesuffix(".png")
@@ -53,9 +64,10 @@ def rename_directory(
 
 def main():
     config = get_config()
+    ids_df = get_ids(config["name_of_file_with_ids"])
     rename_directory(
         config["path_to_folder_with_pictures"],
-        config["name_of_file_with_ids"]
+        ids_df
     )
      
     
