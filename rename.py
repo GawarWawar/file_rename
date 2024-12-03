4fa7b2c16c23cef237e475123719116a6c0a7fbe
GawarWawar <gawarwawar@gmail.com>
2024-12-03 12:42:57 +0200

    feat(start_mode): Introduce Advanced mode: read directory with directories; in each directory look for the ids file; perform separate file rename on each directories.

--- rename.py
+++ rename.py
@@ -80,13 +80,28 @@ def rename_directory(
 def main():
     config = get_config()
     if config["start_mode"] == 0:
-        config = get_config()
+        # Standart mode: read ids from the same directory; look for the folder with images; copy images with new names into result folder.
         ids_df = get_ids(config["name_of_file_with_ids"])
-        files = get_directory(config["path_to_folder"])
+        files = get_directory(config["path_to_folder"], get_all=False)
         rename_directory(
             files,
             ids_df
         )
+    elif config["start_mode"] >= 1:
+        # Advanced mode: read directory with directories; in each directory look for the ids file; perform separate file rename on each directories.
+        high_directory = get_directory(config["path_to_folder"])
+        for item in high_directory:
+            if item.is_dir():
+                try:
+                    ids_df = get_ids(pathlib.Path(item, config["name_of_file_with_ids"]))
+                except FileNotFoundError:
+                    print(f"{item} is a directory. How ever it does not contain file with {config["name_of_file_with_ids"]}")
+                else:
+                    files = get_directory(item, get_all=False)
+                    rename_directory(
+                        files,
+                        ids_df
+                    )   
      
     
                 
