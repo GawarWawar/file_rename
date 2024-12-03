fce9d742f68e3cfc88132f705080263688078646
GawarWawar <gawarwawar@gmail.com>
2024-12-03 21:57:04 +0200

    feat(rename_directory): Make rename_dicectory create zip. It no longer downloads and creates copies of images, it creates zip (or zips) and adds all photos into it. Add new parameters: custom_zip_path - save path for zip; custom_zip_name - name which zip will have. Into start_mode 1 rename_directory uses custom_zip_name to make different zip for every folder

--- rename.py
+++ rename.py
@@ -1,5 +1,6 @@
 import json
 import os
+import zipfile
 import pathlib
 import pandas as pd
 from PIL import Image, UnidentifiedImageError
@@ -51,31 +52,51 @@ def get_directory(path_to_directory: str, get_all: bool = True) -> list[pathlib.
     ]
     
 def rename_directory(
-    files: list[pathlib.Path], 
+    file_pathes: list[pathlib.Path], 
     ids_df: pd.DataFrame,
+    custom_zip_path: pathlib.Path|None = None,
+    custom_zip_name: str|None = None,
 ) -> None:
-    """Find all files in path_to_directory and tries to copy them as images in png format with a new name.
-    Name contain id: from ids DF; part_code: file should have a name which is composed of that part code; and file extension - png.
+    """Find all files in path_to_directory and tries to compress them into zip. Process only images in png format with a new name.
+    New name will contain id: from ids DF; part_code: image should have a name which is composed of that part code; and file extension - .png.
     So images will have a name as: id.part_code.png
 
     Args:
         files (list[pathlib.Path]): List of all files in the directory represented as pathlib.Path
         name_of_file_with_ids (str): Name of file with ids
+        custom_zip_path (pathlib.Path | None, optional): Set in which location zip file will be created. Defaults to None and zip will be located at result folder.
+        custom_zip_name (str | None, optional): Set which name will have zip file after creation. Defaults to None and zip name will be zip_path.name.
     """        
+    
     # For every id we create a copy of each part
-    for id in ids_df.iloc[0]:
-        for file in files:
-            # Each image should have a part code as a name 
-            part_code = file.name.removesuffix(".png")
-            
-            try:
-                # Crate copy of the image with a new name
-                with Image.open(file, "r") as img:
-                    img.save(f"result/{id}.{part_code}.png")
-                    print(f"{id}.{part_code}.png")
-            except UnidentifiedImageError:
-                # Make a warning if file was not an image
-                warnings.warn(f"{file.name} was not an image")
+    if custom_zip_path is None:
+        zip_path = pathlib.Path("result/")
+    else:
+        zip_path = custom_zip_path
+    if custom_zip_name is None:
+        zip_name = zip_path.name
+    else:
+        zip_name = custom_zip_name
+    
+    with zipfile.ZipFile(pathlib.Path(zip_path, f"{zip_name}.zip"), "a") as myzip:  
+        for id in ids_df.iloc[:, 0]:
+            for path in file_pathes:
+                # Process only .png image. 
+                    # Each image should have part_code as a name;
+                    # It will be used as a part of new name.
+                part_code = path.name.removesuffix(".png")
+                if len(path.name) - len(part_code) > 0:
+                    try:
+                        # Insert copy of the file with a new name in the zip
+                        myzip.write(path, f"{id}.{part_code}.png")
+                        # Log which file was added to zip.
+                        print(f"'{id}.{part_code}.png' added to {myzip.filename}")
+                    
+                    except PermissionError:
+                        print("Operation not permitted.")
+                else:
+                    # Log which file was not an image.
+                    print(f"{path.name} is not an png image.")
 
 def main():
     config = get_config()
@@ -100,7 +121,8 @@ def main():
                     file_pathes = get_directory(item, get_all=False)
                     rename_directory(
                         file_pathes,
-                        ids_df
+                        ids_df,
+                        custom_zip_name=file_pathes[0].parent.name
                     )   
      
     
